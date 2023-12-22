a7357988357c12e600ffee70f3ab030dcb781500
Lazizbek <lazizbekabdullayev118@gmail.com>
2023-12-22 14:11:12 +0500

    jmk,l

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,5 @@
 export const api = 'http://192.168.37.66:1328/'
-<<<<<<< HEAD
-=======
+
 // export const api = 'https://f044-188-113-203-39.ngrok-free.app/'
->>>>>>> 2768c96d04e60d9e289b3dbc7f96a96810ce52cf
 
 localStorage.setItem('token' , 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI5OTg5MDAwMDAwMDAiLCJpYXQiOjE3MDI5NzUyMDksImV4cCI6MTcwNTU2NzIwOX0.kT4RJWv22e2Yj8fmN2nj9sO6XxQL_kl4LDDPN40pD8WJ7edaxfN_9ExId2Q4O26pKB33xHOyKSMVfMMag37QBQ')
